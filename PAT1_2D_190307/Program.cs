aaddac245f64abfde28dcde277f8e4847890890b
Ioshua <Idioshua@gmail.com>
2019-03-07 14:50:58 -0300

    Primeira versão do projeto

---
+++ PAT1_2D_190307/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAT1_2D_190307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
